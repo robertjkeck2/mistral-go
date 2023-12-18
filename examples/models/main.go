e02f5aec7cb6b54ea15bcd37bc6716821463a14d
John Keck <robertjkeck2@gmail.com>
2023-12-17 19:51:30 -0600

    List models endpoint example

--- examples/models/main.go
+++ examples/models/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/robertjkeck2/go-mistral"
+)
+
 func main() {
-	// TODO
+	client := mistral.NewMistralClient(os.Getenv("MISTRAL_API_KEY"))
+	resp, err := client.ListModels(context.Background())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if len(resp.Data) == 0 {
+		fmt.Println("No models found")
+		return
+	}
+	for _, model := range resp.Data {
+		fmt.Println(model.ID)
+	}
 }
