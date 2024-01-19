d7b3c3cea4851b71a55f71e6d92cf2fbd8a0f868
Michael Gunkoff <painter@3lines.club>
2024-01-19 16:31:09 +0100

    added roles

---
+++ roles.go
@@ -0,0 +1,9 @@
+package mistral
+
+// Roles are described in the Mistral API documentation:
+// https://docs.mistral.ai/api/#operation/createChatCompletion
+const (
+	RoleSystem    = "system"
+	RoleUser      = "user"
+	RoleAssistant = "assistant"
+)
